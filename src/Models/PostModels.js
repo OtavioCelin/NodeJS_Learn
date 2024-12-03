d295559b5f5261cdf8be85ca6b418e18c7635efb
Otavio Ferreira <otavioferreirapv@gmail.com>
2024-12-03 17:43:17 -0400

    Tirei os dados do meu banco de dados

--- src/Models/PostModels.js
+++ src/Models/PostModels.js
@@ -4,20 +4,20 @@ import { ObjectId } from "mongodb";
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 export async function getTodosPosts() {
-    const db = conexao.db("taviodb");
-    const colecao = db.collection("posts_1");
+    const db = conexao.db("");
+    const colecao = db.collection("");
     return colecao.find().toArray();
 };
 
 export async function criarPosts(novoPost) {
-    const db = conexao.db("taviodb");
-    const colecao = db.collection("posts_1");
+    const db = conexao.db("");
+    const colecao = db.collection("");
     return colecao.insertOne(novoPost);
 };
 
 export async function atualizarPost(id, novoPost) {
-    const db = conexao.db("taviodb");
-    const colecao = db.collection("posts_1");
+    const db = conexao.db("");
+    const colecao = db.collection("");
     const objID = ObjectId.createFromHexString(id); 
     return colecao.updateOne({ _id: new ObjectId (objID) }, { $set: novoPost });
 };
